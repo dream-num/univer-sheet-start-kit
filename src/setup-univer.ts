f584a609c50a49ced7168784d16a2b0c0e3bfe2b
白熱 <sonne@asaki.me>
2024-05-15 17:37:20 +0800

    feat: add univer plugin

--- src/setup-univer.ts
+++ src/setup-univer.ts
@@ -1,10 +1,4 @@
-import '@univerjs/design/lib/index.css'
-import '@univerjs/ui/lib/index.css'
-import '@univerjs/sheets-ui/lib/index.css'
-import '@univerjs/sheets-formula/lib/index.css'
-import '@univerjs/sheets-numfmt/lib/index.css'
-
-import { LocaleType, LogLevel, Univer } from '@univerjs/core'
+import { LocaleType, Univer, UniverInstanceType } from '@univerjs/core'
 import { defaultTheme } from '@univerjs/design'
 import { UniverDocsPlugin } from '@univerjs/docs'
 import { UniverDocsUIPlugin } from '@univerjs/docs-ui'
@@ -15,15 +9,17 @@ import { UniverSheetsFormulaPlugin } from '@univerjs/sheets-formula'
 import { UniverSheetsNumfmtPlugin } from '@univerjs/sheets-numfmt'
 import { UniverSheetsUIPlugin } from '@univerjs/sheets-ui'
 import { UniverUIPlugin } from '@univerjs/ui'
+import { UniverSheetsZenEditorPlugin } from '@univerjs/sheets-zen-editor'
 import { FUniver } from '@univerjs/facade'
-import { locales } from './locale'
+import { enUS } from 'univer:locales'
 
 export function setupUniver() {
   const univer = new Univer({
     theme: defaultTheme,
-    locale: LocaleType.ZH_CN,
-    logLevel: LogLevel.VERBOSE,
-    locales,
+    locale: LocaleType.EN_US,
+    locales: {
+      [LocaleType.EN_US]: enUS,
+    },
   })
 
   univer.registerPlugin(UniverDocsPlugin, {
@@ -34,7 +30,6 @@ export function setupUniver() {
   univer.registerPlugin(UniverUIPlugin, {
     container: 'univer',
     header: true,
-    toolbar: true,
     footer: true,
   })
   univer.registerPlugin(UniverSheetsPlugin)
@@ -44,7 +39,9 @@ export function setupUniver() {
   univer.registerPlugin(UniverFormulaEnginePlugin)
   univer.registerPlugin(UniverSheetsFormulaPlugin)
 
-  univer.createUniverSheet({})
+  univer.registerPlugin(UniverSheetsZenEditorPlugin)
+
+  univer.createUnit(UniverInstanceType.UNIVER_SHEET, {})
 
   return FUniver.newAPI(univer)
 }
